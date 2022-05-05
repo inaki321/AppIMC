0a52cadcf5a42f195b83e9755c18c58e003ec1a8
inaki321 <90475192+inaki321@users.noreply.github.com>
2022-05-04 19:33:59 -0500

    easyphp server

--- serverApps.php
+++ serverApps.php
@@ -1,15 +1,5 @@
 <?php
 
-//conection to db
-$conn = new mysqli("localhost", "root", "", "baseapps");
-  
-if ($conn->connect_error) {
-  echo $conn->connect_error;
-  echo 'ERROR: Unable to connect: .<br>';  
-} 
-else{
-  echo 'Connected to the database.<br>';
-}
 
 
 //Usuario desde kotlin
@@ -21,45 +11,23 @@ $UserToUse ='Juanxito123';
 
 
 //base de datos seleccionar al usuario que se llame $UserToUse y comparar su contrasena 
- 
 
 //Hashear la contrasena para compararla con la que obtuve de la base de datos
+$myObj = new stdClass();
+
 
 $myObj->User = $UserToUse;
 $hashed_password = password_hash($passwordToUse, PASSWORD_DEFAULT);
 $myObj->Password = $hashed_password;
+$cars = array( $myObj, $myObj);
 
-$dbpwd = $hashed_password;
-$myJSON = json_encode($myObj);
 
-//check hashed
-echo $dbpwd . '<br>';
-
-if(password_verify(trim($passwordToUse), $dbpwd))
-{
-    echo 'Bien puto<br>';
-    echo 'Aqui hariamos el select de los datos del usuario <br>';
-}
-else
-{
-    echo 'Mal puto<br> ';
-    echo 'Aqui le ponemos que mal password o username<br> ';
-}
-
+//header("Content-Type: application/json");
+$myJSON = json_encode($cars);
 
 echo $myJSON;
 
 
-/*
-$result = $conn->query("SELECT name FROM employee");
-
-echo "Number of rows: $result->num_rows";
-
-$result->close();
-
-$conn->close();
-*/
-//conection to db
 exit();
 
 ?>
